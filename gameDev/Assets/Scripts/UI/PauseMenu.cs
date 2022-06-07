ad2bf5761c679d0b15107d6f1a028fb7917e2537
rppanya <100025463+rppanya@users.noreply.github.com>
2022-06-07 16:06:44 +0700

    respawn

--- gameDev/Assets/Scripts/UI/PauseMenu.cs
+++ gameDev/Assets/Scripts/UI/PauseMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-
+    public static PauseMenu Instance { get; set; }
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
